05cb86196899080544c0fd79c66035525d8792a0
mar7yk <martykostov@gmail.com>
2020-04-22 00:32:42 +0300

    f

---
+++ susi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  susi
+//
+//  Created by Marty Kostov on 22.04.20.
+//  Copyright © 2020 Marty Kostov. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    
+    return 0;
+}
