fad32a174ccf6cc8758345a1037db1bb845ac93f
mar7yk <martykostov@gmail.com>
2020-04-23 03:54:13 +0300

    tests

--- susi/main.cpp
+++ susi/main.cpp
@@ -12,6 +12,28 @@
 
 int main() {
     
+    SusiDatabase database;
+    
+    database.addProgram("Informatics");
+    
+    database.addCourse("OOP", 1);
+    database.addCourse("VA", 1);
+    database.addCourse("DSTR", 1);
+    
+    database.addCourseToProgram("OOP", "Informatics");
+    database.addCourseToProgram("VA", "Informatics");
+    database.addCourseToProgram("DSTR", "Informatics");
+    
+    
+    database.enroll(45709, "Informatics", 6, "Martin Kostov");
+    
+    database.enrollin(45709, "OOP");
+    database.enrollin(45709, "VA");
+    database.enrollin(45709, "DSTR");
+    
+    database.addGrade(45709, "OOP", 6);
+    
+    database.report(45709);
     
     return 0;
 }
